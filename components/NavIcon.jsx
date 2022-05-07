5f5e74895ec404d30af9e9d71bbbfb998de022c2
Ben <benwestondigital@gmail.com>
2022-05-07 19:10:42 +0100

    hover underline

--- components/NavIcon.jsx
+++ components/NavIcon.jsx
@@ -1,8 +1,8 @@
 const NavIcon = ({ Icon, text }) => {
   return (
-    <div className='text-white flex h-12 w-12 flex-col mx-2 cursor-pointer items-center'>
+    <div className='group text-white flex h-12 w-12 flex-col mx-2 cursor-pointer items-center'>
       <Icon className='h-6 mx-auto ' />
-      <p className="text-xs font-semibold">{text}</p>
+      <p className="group-hover:underline text-xs font-semibold">{text}</p>
     </div>
   );
 };
