063fc65c5dd2830aa53dd0722db9c2422ceb7dcc
Ben <benwestondigital@gmail.com>
2022-05-07 19:13:04 +0100

    footer setup

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div>
+        {/* Payment / Social Links Section */}
+        {/* Site Links Section */}
+        {/* Legal Info Section */}
+    </div>
+  )
+}
+export default Footer
