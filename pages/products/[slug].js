a186d44ab1d79a070507f1cbd813fb663cdba683
Ben <benwestondigital@gmail.com>
2022-05-09 22:19:25 +0100

    add image gallery and furtherinfo component

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -4,6 +4,7 @@ import Price from '../../components/Price';
 import ProductInfo from '../../components/ProductInfo';
 import Purchase from '../../components/Purchase';
 import StoreAvailability from '../../components/StoreAvailability';
+import FurtherInfo from '../../components/FurtherInfo';
 
 const ProductPage = ({
   product: { name, image, flavour, brand, price, numReviews, stars, size },
@@ -15,7 +16,16 @@ const ProductPage = ({
         {/* Left Col - Image */}
         {/* TODO: Make image fixed? */}
         <div className='column1'>
-          <Image alt={name} src={image} width={604} height={604} />
+          <Image alt={name} src={image} width={604} height={604} className='cursor-crosshair'/>
+          <div className='flex items-center justify-between cursor-pointer'>
+            <div className='border-b-4 border-gray-500'>
+              <Image alt={name} src={image} width={88} height={88} />
+            </div>
+            <Image alt={name} src={image} width={88} height={88} />
+            <Image alt={name} src={image} width={88} height={88} />
+            <Image alt={name} src={image} width={88} height={88} />
+            <Image alt={name} src={image} width={88} height={88} />
+          </div>
         </div>
         {/* Right Col - Info */}
         <div className='column2'>
@@ -31,6 +41,7 @@ const ProductPage = ({
           <StoreAvailability />
         </div>
       </div>
+          <FurtherInfo stars={stars} numReviews={numReviews}/>
     </div>
   );
 };
