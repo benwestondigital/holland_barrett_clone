cdb993b6af94f3c9e8e543491026d80368efd86e
Ben <benwestondigital@gmail.com>
2022-05-09 18:59:32 +0100

    split into own component

---
+++ components/StarRating.jsx
@@ -0,0 +1,22 @@
+import { StarIcon } from '@heroicons/react/solid';
+
+const StarRating = ({ stars, numReviews }) => {
+  const fiveStars = [...Array(5)].map(x => 0);
+
+  return (
+    <div className='flex items-center'>
+      {fiveStars.map((s, index) => {
+        return (
+          <StarIcon
+            key={index}
+            className={`h-5 ${
+              stars >= index + 1 ? 'text-yellow-400' : 'text-gray-300'
+            }`}
+          />
+        );
+      })}
+      <span className='text-xs text-gray-400'>({numReviews})</span>
+    </div>
+  );
+};
+export default StarRating;
