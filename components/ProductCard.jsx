fd0cae719e74751d4543c7dd6b1d3e3945c8edc2
Ben <benwestondigital@gmail.com>
2022-05-09 14:48:34 +0100

    fix formatting inc flex grow

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -12,8 +12,10 @@ const ProductCard = ({ data }) => {
         <div className='bg-hbred text-white text-sm font-semibold flex justify-center'>
           <p>Great Savings</p>
         </div>
-        <div className='text-center'>
-          <p className='text-hbgreen font-semibold text-sm'>{data.name}</p>
+        <div className='px-2 flex flex-col items-center justify-between flex-grow'>
+          <p className='text-hbgreen font-semibold text-sm mt-2 text-center'>
+            {data.name}
+          </p>
           <p>
             Star Rating Here{' '}
             <span className='text-xs text-gray-400'>({data.numReviews})</span>
@@ -22,9 +24,9 @@ const ProductCard = ({ data }) => {
           <p className='text-sm text-gray-400'>
             {pricePer100g(data.price, data.size)}
           </p>
-          <button className='rounded-3xl bg-hbgreen hover:bg-[#00322B] hover:drop-shadow-md text-white py-2 px-8 font-semibold text-sm my-4'>
-            Add to Basket
-          </button>
+            <button className='rounded-3xl bg-hbgreen hover:bg-[#00322B] hover:drop-shadow-md text-white py-2 px-8 font-semibold text-sm my-4 w-11/12'>
+              Add to Basket
+            </button>
         </div>
       </div>
     </Link>
