4b78fed4137c4f4157790cebec4aa52d765dd470
Ben <benwestondigital@gmail.com>
2022-05-09 15:23:26 +0100

    remove margin top

--- components/Products.jsx
+++ components/Products.jsx
@@ -2,7 +2,7 @@ import ProductCard from '../components/ProductCard';
 
 const Products = ({ products }) => {
   return (
-    <div className='col-span-4 px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 mt-8'>
+    <div className='col-span-4 px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2'>
       {products.map(prod => {
         return <ProductCard key={prod.id} data={prod} />;
       })}
