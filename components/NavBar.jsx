5fc2f393814a38648405c01467773743c0f2e7a2
Ben <benwestondigital@gmail.com>
2022-05-09 15:54:53 +0100

    add basket items number

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -14,7 +14,17 @@ const NavBar = () => {
       <NavIcon Icon={LocationMarkerIcon} text='Store' />
       <NavIcon Icon={GiftIcon} text='Rewards' />
       <NavIcon Icon={HeartIcon} text='Favourites' />
-      <NavIcon Icon={BsBasket3} text='Basket' />
+
+      <div className='group text-white flex h-12 w-12 flex-col mx-2 cursor-pointer items-center relative'>
+        <BsBasket3 size={20} className='h-6 mx-auto ' />
+        <p className='group-hover:underline text-xs font-semibold'>Basket</p>
+        <div className='absolute top-0 left-6 bg-hbred h-4 w-4 rounded-full'>
+          {/* TODO: Use number of items in basket for this number */}
+          <p className='absolute bottom-[-1.5px] right-1 text-sm font-semibold'>
+            1
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
