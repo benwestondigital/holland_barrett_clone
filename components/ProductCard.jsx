53de460c69088de6875ae7518b9af4b4b77e332a
Ben <benwestondigital@gmail.com>
2022-05-09 19:00:04 +0100

    destructure params and split starrating into comp

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,44 +1,29 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { displayPrice, pricePer100g } from '../utils';
-import { StarIcon } from '@heroicons/react/solid';
+import StarRating from './StarRating';
 
-const ProductCard = ({ data }) => {
-  const productLink = data.name.replace(' ', '');
-  const fiveStars = [...Array(5)].map(x => 0);
+const ProductCard = ({
+  data: { name, image, dealType, stars, numReviews, price, size },
+}) => {
+  const productLink = name.replace(' ', '');
 
   return (
     <Link href={`products/${productLink}`}>
       <div className='border flex flex-col cursor-pointer'>
         <div data-test='product-image'>
-          <Image alt={data.name} src={data.image} width={302} height={302} />
+          <Image alt={name} src={image} width={302} height={302} />
         </div>
         <div className='bg-hbred text-white text-sm font-semibold flex justify-center py-1'>
-          <p>{data.dealType}</p>
+          <p>{dealType}</p>
         </div>
         <div className='px-2 flex flex-col items-center justify-between flex-grow'>
           <p className='text-hbgreen font-semibold text-sm mt-2 text-center'>
-            {data.name}
+            {name}
           </p>
-          <div className='flex items-center'>
-            {fiveStars.map((s, index) => {
-              return (
-                <StarIcon
-                  key={index}
-                  className={`h-5 ${
-                    data.stars >= index + 1
-                      ? 'text-yellow-400'
-                      : 'text-gray-300'
-                  }`}
-                />
-              );
-            })}
-            <span className='text-xs text-gray-400'>({data.numReviews})</span>
-          </div>
-          <p className='font-bold'>{displayPrice(data.price)}</p>
-          <p className='text-sm text-gray-400'>
-            {pricePer100g(data.price, data.size)}
-          </p>
+          <StarRating stars={stars} numReviews={numReviews} />
+          <p className='font-bold'>{displayPrice(price)}</p>
+          <p className='text-sm text-gray-400'>{pricePer100g(price, size)}</p>
           <button className='rounded-3xl bg-hbgreen hover:bg-[#00322B] hover:drop-shadow-md text-white py-2 px-8 font-semibold text-sm my-4 w-11/12'>
             Add to Basket
           </button>
