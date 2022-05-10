fe48a570971b953355ddcf446716eb1f51c0e8ac
Ben <benwestondigital@gmail.com>
2022-05-10 16:19:16 +0100

    change text colour

--- components/EmptyBasket.jsx
+++ components/EmptyBasket.jsx
@@ -1,10 +1,10 @@
 const EmptyBasket = () => {
   return (
-    <div className='bg-[#eeeeef] h-96 p-6'>
+    <div className='bg-[#eeeeef] h-96 p-6 '>
     <div className='p-2 container'>
       <h2 className='font-semibold text-3xl pt-12 pl-12'>Your basket</h2>
       <div className='bg-white p-12 m-8 flex items-center justify-center'>
-        <p className='text-xl'>You have no products in your basket</p>
+        <p className='text-xl text-[#333]'>You have no products in your basket</p>
       </div>
     </div>
   </div>
