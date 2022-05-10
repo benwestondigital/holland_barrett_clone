4eb48430ed7a63dac9f91fcaa63d3efa78d4fcc0
Ben <benwestondigital@gmail.com>
2022-05-10 15:26:44 +0100

    link to basket page

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,6 +8,7 @@ import { BsBasket3 } from 'react-icons/bs';
 import NavIcon from './NavIcon';
 import { BasketContext } from '../context/Basket';
 import { useContext } from 'react';
+import Link from 'next/link';
 
 const NavBar = () => {
   const { basket } = useContext(BasketContext);
@@ -20,17 +21,19 @@ const NavBar = () => {
       <NavIcon Icon={GiftIcon} text='Rewards' />
       <NavIcon Icon={HeartIcon} text='Favourites' />
 
-      <div className='group text-white flex h-12 w-12 flex-col mx-2 cursor-pointer items-center relative'>
-        <BsBasket3 size={20} className='h-6 mx-auto ' />
-        <p className='group-hover:underline text-xs font-semibold'>Basket</p>
-        {basketItems > 0 && (
-          <div className='absolute top-0 left-6 bg-hbred h-4 w-4 rounded-full'>
-            <p className='absolute bottom-[-1.5px] right-1 text-sm font-semibold'>
-              {basketItems}
-            </p>
-          </div>
-        )}
-      </div>
+      <Link href='/basket'>
+        <div className='group text-white flex h-12 w-12 flex-col mx-2 cursor-pointer items-center relative'>
+          <BsBasket3 size={20} className='h-6 mx-auto ' />
+          <p className='group-hover:underline text-xs font-semibold'>Basket</p>
+          {basketItems > 0 && (
+            <div className='absolute top-0 left-6 bg-hbred h-4 w-4 rounded-full'>
+              <p className='absolute bottom-[-1.5px] right-1 text-sm font-semibold'>
+                {basketItems}
+              </p>
+            </div>
+          )}
+        </div>
+      </Link>
     </div>
   );
 };
