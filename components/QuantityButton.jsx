e994b5a8f6840d77a892639172d73f293537aedf
Ben <benwestondigital@gmail.com>
2022-05-10 17:19:37 +0100

    split quantity button to component

---
+++ components/QuantityButton.jsx
@@ -0,0 +1,31 @@
+const QuantityButton = ({ decreaseQuantity, increaseQuantity, quantity }) => {
+  return (
+    <div className='m-2 grid grid-cols-2 gap-x-1 pt-2'>
+      <label htmlFor='quantity' className='text-xl font-semibold text-black'>
+        Quantity:
+      </label>
+      <div className='border rounded-lg w-11/12 flex items-center justify-between bg-white px-2'>
+        <button
+          className='text-lg h-8 text-hbgreen'
+          onClick={e => decreaseQuantity(e)}
+        >
+          -
+        </button>
+        <input
+          type='number'
+          id='quantity'
+          className='w-14 h-8 no-spin border-0 text-center'
+          value={quantity}
+          readOnly
+        />
+        <button
+          className='text-lg h-8 text-hbgreen'
+          onClick={e => increaseQuantity(e)}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+export default QuantityButton;
